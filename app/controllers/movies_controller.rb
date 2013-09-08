c4c6883bb9f19cae6f034d773df6a30ee2f80426
Deepak Verma <deepak.23.verma@gmail.com>
2013-09-07 23:40:27 -0400

    adding and viewing reviews works

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,8 +13,12 @@ class MoviesController < ApplicationController
   
   def update
     @movie = Movie.find(params[:id])
-    @movie.update(movie_params)
-    redirect_to @movie
+    if @movie.update(movie_params)
+      flash[:notice] = "Movie successfully updted!"
+      redirect_to @movie
+    else
+      render :edit
+    end
   end
 
   def new
@@ -23,13 +27,19 @@ class MoviesController < ApplicationController
   
   def create
     @movie = Movie.new(movie_params)
-    @movie.save
-    redirect_to @movie
+    if @movie.save
+      flash[:notice] = "Movie successfully created!"
+      redirect_to @movie
+    else
+      render :new
+    end
+
   end
   
   def destroy
     @movie = Movie.find(params[:id])
     @movie.destroy
+    flash[:alert] = "Movie DESTROYED!"
     redirect_to movies_url
   end
   
@@ -38,5 +48,6 @@ private
   def movie_params
     params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
   end
+
 end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,6 +10,8 @@ class Movie < ActiveRecord::Base
   RATINGS = %w(G PG PG-13 R NC-17)
   validates :rating, inclusion: %w(
     G PG PG-13 R NC-17)
+  
+  has_many :reviews, dependent: :destroy
 
   def self.released
     where("released_on <= ?", Time.now).order("released_on desc")
